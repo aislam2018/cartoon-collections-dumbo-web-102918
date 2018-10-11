7fe26892b30b073ba266dfe8a0823b9c41e79dbf
anik1606 <anik1606@gmail.com>
2018-10-11 21:16:33 +0000

    automatically backed up by learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -10,8 +10,8 @@ def summon_captain_planet(arr)
   arr.collect { |ele| ele.capitalize + "!"}
 end
 
-def long_planeteer_calls# code an argument here
-  # Your code here
+def long_planeteer_calls(arr)
+  arr.any? {|ele| ele.size >= 4
 end
 
 def find_the_cheese# code an argument here
